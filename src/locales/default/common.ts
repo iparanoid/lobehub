5b4ca733cf2fe3a13c1a92e5f55043edbd1aac29
rdmclin2 <rdmclin2@gmail.com>
2026-02-01 18:26:49 +0800

    fix: suggest questions skeleton

--- src/locales/default/common.ts
+++ src/locales/default/common.ts
@@ -327,6 +327,7 @@ export default {
   'geminiImageChineseWarning.doNotShowAgain': 'Do Not Show Again',
   'geminiImageChineseWarning.title': 'Chinese Input Notice',
   'historyRange': 'History Range',
+  'home.suggestQuestions': 'Try these examples',
   'import': 'Import',
   'importData': 'Import Data',
   'importModal.error.desc':
